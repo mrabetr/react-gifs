32af1f5a35d8de200eaa372809e65b2b84256b8e
rafik mrabet <mrabetr@hotmail.com>
2022-10-21 17:00:20 +0100

    updates to the Hello component

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello, {name}
+//     </div>
+//   );
+// };
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+    // this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      clicked: false,
+      counter: 0
+    };
+  }
+
+  // handleClick() {
+  //   this.setState({
+  //     clicked: !this.state.clicked
+  //   });
+  // }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    });
+  }
+
+  render() {
+    // console.log('counter', this.state.counter);
+    return (
+      <div
+        className={this.state.clicked ? "clicked" : ""}
+        onClick={this.handleClick}
+        // onClick={this.handleClick.bind(this)}
+      >
+       Hello, {this.props.name} {this.state.counter}
+     </div>
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="Rafik" />, root);
 }
